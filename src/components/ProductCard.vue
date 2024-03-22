782302cf048ffe5a3528e96b0b222304aff612e7
Carlo-Adile <adile.carlo.a@gmailcom>
2024-03-22 16:58:42 +0100

    add function for discounted price

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -5,6 +5,21 @@ export default {
   name: 'ProductCard',
   props: {
     product: Object
+  },
+  computed:{
+    hasDiscount() {
+      return this.product.badges.some(badge => badge.type === 'discount')
+    },
+    discountedPrice(){
+      if(this.hasDiscount){
+        const discountBadge = this.product.badges.find(badge => badge.type === 'discount');
+        const discountPercentage = parseFloat(discountBadge.value) / 100;
+        return (this.product.price * (1 - discountPercentage)).toFixed(2);
+      }
+      else{
+        return this.product.price;
+      }
+    }
   }
 }
 
@@ -20,7 +35,10 @@ export default {
         <img :src="product.frontImage" alt="product.name" class="card-image">
       </div>
       <div class="card-body">
-        <h4><!--  {{ product.name }} --> ciao</h4>
+        <p> {{ product.brand }}</p>
+        <p> {{ product.name }}</p>
+        <p v-if="hasDiscount"> {{ discountedPrice }} €</p>
+        <p v-else> {{ product.price }} €</p>
       </div>
     </div>
   </div>
@@ -37,17 +55,14 @@ export default {
 
 .card {
   margin:4px;
-  border: 1px solid black;
   border-radius: 8px;
   padding: 8px;
   margin-bottom: 8px;
-  overflow: hidden;
 }
 
 .card-header {
   width: 100%;
-  border-bottom: 1px solid #ccc;
-
+  margin-bottom: -6px;
   img {
     width: 100%;
     object-fit: cover;
@@ -57,6 +72,7 @@ export default {
 .card-body{
   color:black;
   padding: 6px;
+  line-height: 0.5rem;
 }
 
 </style>
