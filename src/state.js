53e708825df87d14f15474013b157caa1b19f621
Carlo-Adile <adile.carlo.a@gmailcom>
2024-03-26 16:03:31 +0100

    created an array of the object state to fetch my products from my api

--- src/state.js
+++ src/state.js
@@ -1,13 +1,32 @@
 import { reactive } from 'vue';
+import axios from 'axios';
 
+/* export global state as an object */
 export const state = reactive ({
 
   message: 'this is a test',
 
-  myMethod(){
-    // Actions that change the state (your methods)
+  /* store products here, this is an array of the object state */
+  products: [],
+  base_products_api_url: 'http://localhost:3000/products',
+
+
+  /* fetch products from API */
+  getProducts: async function(){
+    try{
+      const response = await axios.get(this.base_products_api_url);
+      this.products = response.data;
+    }
+    catch (error) {
+      console.log('errore durante il Fetch', error.message)
+    }
+  }
+
+  // Actions that change the state (your methods)
+  /* myMethod(){
+    
   },
   myOtherMethod(){
-    
-  }
+
+  } */
 })
